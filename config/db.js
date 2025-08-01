53c65e03a7ed9ac3dded6b87dd01be22446b1d8d
vinicius-bispo1 <vini.bispo125@gmail.com>
2025-07-31 21:35:05 -0300

    fix: bug

--- config/db.js
+++ config/db.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // importante para funcionar no Render
+  },
 });
 
 module.exports = pool;
